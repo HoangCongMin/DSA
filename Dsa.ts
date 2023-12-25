dfcf663003568784ff878acff7d31ef7f3036bd0
hoang cong minh <0782708546hoangcongminh@gmail.com>
2023-12-26 01:35:48 +0700

    DSA START

---
+++ Dsa.ts
@@ -0,0 +1,16 @@
+//bai 1
+console.log("hello world!");
+//bai 2
+const a : string = "hello world";
+console.log(a);
+//bai 3
+const b : string = "hello";
+const c : string = "world";
+const d : string = b + " "+ c;
+console.log(d);
+//bai 4
+const print_ = (input:string) => console.log(input)
+print_(d);
+// thầy nhớ chạy npm chạy trên terminal trước nha
+// thầy dùng lệnh này để chạy trên terminal nha 
+// npx ts-node Dsa.ts
